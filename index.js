0a4cdec633775be447bb315b9b52be1f8ec16961
Leonard Martin <leonard.martin@gmail.com>
2018-03-09 10:50:28 +0000

    Increase body size limit (#16)
    * Increase body size limit
    
    The default limit of the json body parser is 100kb, which does not allow for rendering large documents.
    
    Increase the limit to 2mb to be in line with the default nginx body size limit.
    
    * Make body size limit configurable
    
    Allow the default value of 2mb to be overridden if required with an environment variable

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const errorHandler = require('./middleware/error-handler');
 
 app.use(churchill(logger));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: config.limit }));
 
 app.use('/convert', controller);
 app.use(errorHandler);
